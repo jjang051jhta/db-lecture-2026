1e7ed1e6a1e1d9971a178a4caed5df7e4d7dcf1f
jjang051 <tjjjang051@nate.com>
2026-05-13 10:23:47 +0900

    연산자

--- select01.sql
+++ select01.sql
@@ -3,3 +3,73 @@ SELECT * FROM emp; --crud
 SELECT empno, ename, job FROM emp;
 SELECT * FROM dept;
 SELECT * FROM salgrade;
+
+SELECT DISTINCT job, deptno FROM emp;
+SELECT ALL job,deptno FROM emp;
+
+-- 별칭(alias)를 정할때는 as 를 쓴다.
+SELECT ename AS name,sal*12 AS annsal FROM emp;
+
+--order by 순서 정하기 오름 차순/내림차순 
+SELECT * FROM emp ORDER BY sal desc;
+--1 empno 기준으로 내림 차순 정렬
+SELECT * FROM emp ORDER BY empno desc;
+SELECT ename,sal, deptno FROM emp ORDER BY deptno,sal desc;
+
+--answer01
+SELECT empno AS 사원번호, ename AS 사원이름 FROM emp;
+
+--answer02
+SELECT  distinct job FROM emp;
+
+--answer03
+SELECT 
+	empno AS employee_no, 
+	ename AS employee_name, 
+	mgr AS manager, 
+	sal AS salary, 
+	comm AS commission, 
+	deptno AS department_no
+FROM emp;
+
+SELECT * FROM emp 
+WHERE deptno = 30;
+
+
+SELECT * FROM emp
+WHERE empno = 7369;
+
+-- 값의 경우는 대소문자 구분
+SELECT * FROM emp
+WHERE deptno = 30 AND job = 'SALESMAN';
+
+-- 사원번호가 7499이고 부서번호가 30인 사람 출력
+SELECT * FROM emp
+WHERE deptno = 30 AND empno = 7499;
+
+SELECT * FROM emp
+WHERE deptno = 30 OR job = 'CLERK';
+
+SELECT * FROM emp
+WHERE NOT deptno = 20;
+
+-- 산술연산자
+SELECT * FROM EMP
+WHERE sal*12 = 36000;
+
+SELECT * FROM EMP
+WHERE sal+sal+sal+sal = 12000;
+
+SELECT sal , sal+1000 AS plus_sal, sal*12 AS annsal FROM emp;
+
+--null은 연산이 안됨
+SELECT ename,comm+1000 AS plus_comm FROM emp;
+
+--null은 연산이 안됨
+SELECT ename,nvl(comm,0)+1000 AS plus_comm FROM emp;
+
+
+
+
+
+
---
+++ select02.sql
@@ -0,0 +1,41 @@
+SELECT ename,empno,job,mgr FROM emp;
+SELECT * FROM emp;
+SELECT * FROM emp 
+WHERE empno = 7369 OR ename = 'SMITH';
+-- 연산자
+--1. 비교연산자
+SELECT * FROM EMP
+WHERE sal >= 3000
+ORDER BY sal DESC, empno ASC;
+
+SELECT * FROM emp
+WHERE sal <= 3000
+ORDER BY sal;
+
+SELECT * FROM emp
+WHERE ename >= 'F';
+
+SELECT * FROM emp
+WHERE ename = 'SMITH';
+
+--다르다
+SELECT * FROM emp
+WHERE sal != 3000;
+
+SELECT * FROM emp
+WHERE sal <> 3000;
+
+SELECT * FROM emp
+WHERE sal ^= 3000;
+--quiz job이 MANAGER 또는 SALESMAN 또는 CLERK
+SELECT * FROM emp
+WHERE job = 'MANAGER' OR job = 'SALESMAN' OR job = 'CLERK'
+ORDER BY job;
+
+SELECT * FROM emp
+WHERE job IN ('MANAGER','SALESMAN','CLERK')
+ORDER BY job;
+
+
+
+
